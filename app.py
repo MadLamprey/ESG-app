f4ca408587de82d6362f420fdca5f93c9a55bc2b
Misra Aditya <e1096355@u.nus.edu>
2024-07-27 01:09:26 +0800

    Update dashboard

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import altair as alt
 import boto3
 import base64
 import random
+import json
 from datetime import datetime
 from newsapi import NewsApiClient
 from streamlit_option_menu import option_menu
@@ -12,7 +13,6 @@ newsapi = NewsApiClient(api_key='172888f1419444b7aa01139b6309fad8')
 session = boto3.session.Session('us-east-1')
 bedrock_agent_client = boto3.client('bedrock-agent', region_name='us-east-1')
 bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime', region_name='us-east-1', aws_access_key_id='AKIAQ3EGSKIHRPD4V56K', aws_secret_access_key='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')
-
 
 def get_base64(bin_file):
     with open(bin_file, 'rb') as f:
@@ -160,17 +160,16 @@ def fetch_answer(question):
                 raise Exception("unexpected event.", event)
     except Exception as e:
         raise Exception("unexpected event.",e)
-    prev_esg_score = random.randint(0, 100)
-    evaluated_esg_score = random.randint(0, 100)
-    analysis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc tincidunt aliquam. Nullam nec purus nec nunc tincidunt aliquam. Nullam nec purus nec nunc tincidunt aliquam. Nullam nec purus nec nunc tincidunt aliquam."
-
-    arrow_class = ""
-    if evaluated_esg_score > prev_esg_score:
-        arrow_class = "up"
-    elif evaluated_esg_score < prev_esg_score:
-        arrow_class = "down"
-
-    return prev_esg_score, evaluated_esg_score, analysis, arrow_class, final_answer
+    
+    col1, col2 = st.columns([2, 2])
+    with col1:
+        st.markdown(f"<div class='stChatMessage'>{question}</div>", unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"<div class='stChatMessage'>{final_answer}</div>", unsafe_allow_html=True)
+        
+    st.markdown("<script>scrollToBottom();</script>", unsafe_allow_html=True)
+    # return final_answer without sources
+    return final_answer[:(final_answer.find('<sources>') - 10)]
 
 def main_page():
     title = f"<h1 class='header'>Trailblazers ESG Analyzer</h1></br><p class='desc'>Gain comprehensive ESG insights for companies that you are interested in, empowering you to make informed decisions. Start a chat below to learn more!</p></br>"
@@ -203,26 +202,8 @@ def main_page():
     if prompt:
         question = prompt
         st.session_state.messages.append({"role": "user", "content": question})
-        prev_score, new_score, analysis, arrow_class, final_answer = fetch_answer(question)
-        arrow_html = f"<i class='arrow {arrow_class}'></i>" if arrow_class else ""
-        answer = f"""
-        <div style="margin-top: 10px;">
-            <div class="answer-score">
-            <p class="esg-score"><strong>Computed ESG Score:  </strong> {new_score}  {arrow_html} </p>
-            <p>[prev: {prev_score}]</p>
-            </div>
-            <p><strong>Analysis:</strong> {analysis}</p>
-        </div>
-        """
+        final_answer = fetch_answer(question)
         st.session_state.messages.append({"role": "assistant", "content": final_answer})
-
-        col1, col2 = st.columns([2, 2])
-        with col1:
-            st.markdown(f"<div class='stChatMessage'>{question}</div>", unsafe_allow_html=True)
-        with col2:
-            st.markdown(f"<div class='stChatMessage'>{final_answer}</div>", unsafe_allow_html=True)
-        
-        st.markdown("<script>scrollToBottom();</script>", unsafe_allow_html=True)
 
     if uploaded_files:
         uploaded_file_names = [uploaded_file.name for uploaded_file in uploaded_files]
@@ -246,7 +227,58 @@ def fetch_trending_news(stocks):
     news = sorted(news, key=lambda x: x['publishedAt'], reverse=True)
     return news
 
-def portfolio_page(curr_score, prev_score):
+def fetch_data(dashboard_agent_runtime_client):
+    response = dashboard_agent_runtime_client.invoke_agent(
+        inputText="Get my portfolio",
+        agentId='O7WDUFVTUK',
+        agentAliasId='TEYUNEU0XC',
+        sessionId='1234'
+    )
+    event_stream = response['completion']
+    final_answer = None
+    try:
+        for event in event_stream:
+            if 'chunk' in event:
+                data = event['chunk']['bytes']
+                final_answer = data.decode('utf8')
+            elif 'trace' in event:
+                print(event['trace'])
+            else: 
+                raise Exception("unexpected event.", event)
+    except Exception as e:
+        raise Exception("unexpected event.", e)
+    
+    return final_answer
+
+def portfolio_page():
+    response = None
+    stocks = ['Disney', 'Apple', 'Tesla', 'Microsoft']
+    e_scores = [random.randint(0, 100) for _ in range(4)]
+    s_scores = [random.randint(0, 100) for _ in range(4)]
+    g_scores = [random.randint(0, 100) for _ in range(4)]
+    overall_e_score = random.randint(0, 100)
+    overall_s_score = random.randint(0, 100)
+    overall_g_score = random.randint(0, 100)
+
+    if "res_e" not in st.session_state:
+        st.session_state.res_e = None
+    if "res_s" not in st.session_state:
+        st.session_state.res_s = None
+    if "res_g" not in st.session_state:
+        st.session_state.res_g = None
+
+    prompt = st.chat_input("What is up?")
+    if prompt:
+        dashboard_agent_runtime_client = boto3.client('bedrock-agent-runtime', region_name='us-east-1', aws_access_key_id='AKIAQ3EGSKIHRPD4V56K', aws_secret_access_key='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')
+        response = json.loads(fetch_data(dashboard_agent_runtime_client))
+        stocks = list(map(lambda n: n['company'], response['individual_companies']))
+        e_scores = list(map(lambda n: round(n['environmental_score']), response['individual_companies']))
+        s_scores = list(map(lambda n: round(n['social_score']), response['individual_companies']))
+        g_scores = list(map(lambda n: round(n['governance_score']), response['individual_companies']))
+        overall_e_score = response['portfolio']['environmental_score']
+        overall_s_score = response['portfolio']['social_score']
+        overall_g_score = response['portfolio']['governance_score']
+
     title = f"<h1 class='header'>My Portfolio</h1></br>"
     st.write(title, unsafe_allow_html=True)
     st.divider()
@@ -266,7 +298,11 @@ def portfolio_page(curr_score, prev_score):
             unsafe_allow_html=True
         )
     with cols[1]:
-        cols[1].metric("Environment", curr_score['e'], curr_score['e'] - prev_score['e'])
+        if st.session_state.res_e:
+            cols[1].metric("Environment", overall_e_score, overall_e_score - st.session_state.res_e)
+        else:
+            cols[1].metric("Environment", overall_e_score)
+            st.session_state.res_e = overall_e_score
     with cols[2]:
         st.markdown(
             '''
@@ -282,7 +318,11 @@ def portfolio_page(curr_score, prev_score):
             unsafe_allow_html=True
         )
     with cols[3]:
-        cols[3].metric("Social", curr_score['s'], curr_score['s'] - prev_score['s'])
+        if st.session_state.res_s:
+            cols[3].metric("Social", overall_s_score, overall_s_score - st.session_state.res_s)
+        else:
+            cols[3].metric("Social", overall_s_score)
+            st.session_state.res_s = overall_s_score
     with cols[4]:
         st.markdown(
             '''
@@ -298,19 +338,25 @@ def portfolio_page(curr_score, prev_score):
             unsafe_allow_html=True
         )
     with cols[5]:
-        cols[5].metric("Governance", curr_score['g'], curr_score['g'] - prev_score['g'])
+        if st.session_state.res_g:
+            cols[5].metric("Governance", overall_g_score, overall_g_score - st.session_state.res_g)
+        else:
+            cols[5].metric("Governance", overall_g_score)
+            st.session_state.res_g = overall_g_score
     
     st.divider()
 
-    stocks = ["AAPL", "GOOGL", "AMZN", "MSFT", "TSLA"]
     data = {
         "Stocks": stocks,
-        "Environment": [random.randint(0, 100) for _ in range(5)],
-        "Social": [random.randint(0, 100) for _ in range(5)],
-        "Governance": [random.randint(0, 100) for _ in range(5)]
+        "Environment": e_scores,
+        "Social": s_scores,
+        "Governance": g_scores
     }
 
-    cols = st.columns([5, 1, 2])
+    if "data" not in st.session_state:
+        st.session_state.data = data
+
+    cols = st.columns([5, 1, 5])
     with cols[0]:
         st.table(data)
 
@@ -395,9 +441,7 @@ def main():
     if selection == "Home":
         main_page()
     elif selection == "My Portfolio":
-        curr_score = {"e": random.randint(0, 100), "s": random.randint(0, 100), "g": random.randint(0, 100)}
-        prev_score = {"e": random.randint(0, 100), "s": random.randint(0, 100), "g": random.randint(0, 100)}
-        portfolio_page(curr_score, prev_score)
+        portfolio_page()
 
 if __name__ == "__main__":
     main()
