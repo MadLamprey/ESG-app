0eaccb9551b3a66f5cb80726550fd636dd822f48
Misra Aditya <e1096355@u.nus.edu>
2024-07-27 01:33:54 +0800

    Debug

--- app.py
+++ app.py
@@ -251,181 +251,182 @@ def fetch_data(dashboard_agent_runtime_client):
     return final_answer
 
 def portfolio_page():
-    response = None
-    stocks = ['Disney', 'Apple', 'Tesla', 'Microsoft']
-    e_scores = [random.randint(0, 100) for _ in range(4)]
-    s_scores = [random.randint(0, 100) for _ in range(4)]
-    g_scores = [random.randint(0, 100) for _ in range(4)]
-    overall_e_score = random.randint(0, 100)
-    overall_s_score = random.randint(0, 100)
-    overall_g_score = random.randint(0, 100)
+    try:
+        response = None
+        stocks = ['Disney', 'Apple', 'Tesla', 'Microsoft']
+        e_scores = [random.randint(0, 100) for _ in range(4)]
+        s_scores = [random.randint(0, 100) for _ in range(4)]
+        g_scores = [random.randint(0, 100) for _ in range(4)]
+        overall_e_score = random.randint(0, 100)
+        overall_s_score = random.randint(0, 100)
+        overall_g_score = random.randint(0, 100)
 
-    if "res_e" not in st.session_state:
-        st.session_state.res_e = None
-    if "res_s" not in st.session_state:
-        st.session_state.res_s = None
-    if "res_g" not in st.session_state:
-        st.session_state.res_g = None
+        if "res_e" not in st.session_state:
+            st.session_state.res_e = None
+        if "res_s" not in st.session_state:
+            st.session_state.res_s = None
+        if "res_g" not in st.session_state:
+            st.session_state.res_g = None
 
-    prompt = st.chat_input("What is up?")
-    if prompt:
-        dashboard_agent_runtime_client = boto3.client('bedrock-agent-runtime', region_name='us-east-1', aws_access_key_id='AKIAQ3EGSKIHRPD4V56K', aws_secret_access_key='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')
-        try:
+        prompt = st.chat_input("What is up?")
+        if prompt:
+            dashboard_agent_runtime_client = boto3.client('bedrock-agent-runtime', region_name='us-east-1', aws_access_key_id='AKIAQ3EGSKIHRPD4V56K', aws_secret_access_key='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')
             response = json.loads(fetch_data(dashboard_agent_runtime_client))
-        except Exception as e:
-            print(e)
-        stocks = list(map(lambda n: n['company'], response['individual_companies']))
-        e_scores = list(map(lambda n: round(n['environmental_score']), response['individual_companies']))
-        s_scores = list(map(lambda n: round(n['social_score']), response['individual_companies']))
-        g_scores = list(map(lambda n: round(n['governance_score']), response['individual_companies']))
-        overall_e_score = response['portfolio']['environmental_score']
-        overall_s_score = response['portfolio']['social_score']
-        overall_g_score = response['portfolio']['governance_score']
+            
+            stocks = list(map(lambda n: n['company'], response['individual_companies']))
+            e_scores = list(map(lambda n: round(n['environmental_score']), response['individual_companies']))
+            s_scores = list(map(lambda n: round(n['social_score']), response['individual_companies']))
+            g_scores = list(map(lambda n: round(n['governance_score']), response['individual_companies']))
+            overall_e_score = response['portfolio']['environmental_score']
+            overall_s_score = response['portfolio']['social_score']
+            overall_g_score = response['portfolio']['governance_score']
 
-    title = f"<h1 class='header'>My Portfolio</h1></br>"
-    st.write(title, unsafe_allow_html=True)
-    st.divider()
-    cols = st.columns([1, 5, 1, 5, 1, 5])
-    with cols[0]:
-        st.markdown(
-            '''
-                <div class="divider-vertical-line"></div>
-                <style>
-                    .divider-vertical-line {
-                        border-right: 2px solid white;
-                        height: 320px;
-                        margin: auto;
-                    }
-                </style>
-            ''',
-            unsafe_allow_html=True
-        )
-    with cols[1]:
-        if st.session_state.res_e:
-            cols[1].metric("Environment", overall_e_score, overall_e_score - st.session_state.res_e)
-        else:
-            cols[1].metric("Environment", overall_e_score)
-            st.session_state.res_e = overall_e_score
-    with cols[2]:
-        st.markdown(
-            '''
-                <div class="divider-vertical-line"></div>
-                <style>
-                    .divider-vertical-line {
-                        border-right: 2px solid white;
-                        height: 320px;
-                        margin: auto;
-                    }
-                </style>
-            ''',
-            unsafe_allow_html=True
-        )
-    with cols[3]:
-        if st.session_state.res_s:
-            cols[3].metric("Social", overall_s_score, overall_s_score - st.session_state.res_s)
-        else:
-            cols[3].metric("Social", overall_s_score)
-            st.session_state.res_s = overall_s_score
-    with cols[4]:
-        st.markdown(
-            '''
-                <div class="divider-vertical-line"></div>
-                <style>
-                    .divider-vertical-line {
-                        border-right: 2px solid white;
-                        height: 90px;
-                        margin: auto;
-                    }
-                </style>
-            ''',
-            unsafe_allow_html=True
-        )
-    with cols[5]:
-        if st.session_state.res_g:
-            cols[5].metric("Governance", overall_g_score, overall_g_score - st.session_state.res_g)
-        else:
-            cols[5].metric("Governance", overall_g_score)
-            st.session_state.res_g = overall_g_score
-    
-    st.divider()
-
-    data = {
-        "Stocks": stocks,
-        "Environment": e_scores,
-        "Social": s_scores,
-        "Governance": g_scores
-    }
-
-    if "data" not in st.session_state:
-        st.session_state.data = data
-
-    cols = st.columns([5, 1, 5])
-    with cols[0]:
-        st.table(data)
-
-        source = alt.pd.DataFrame(data)
-        bars_e = alt.Chart(source).mark_bar().encode(
-            x='Stocks',
-            y='Environment'
-        ).properties(
-            title="Environment",
-            background="transparent",
-            height=200
-        ).configure_axis(disable=True)
-        bars_s = alt.Chart(source).mark_bar().encode(
-            x='Stocks',
-            y='Social'
-        ).properties(
-            title="Social",
-            background="transparent",
-            height=200
-        ).configure_axis(disable=True)
-        bars_g = alt.Chart(source).mark_bar().encode(
-            x='Stocks',
-            y='Governance'
-        ).properties(
-            title="Governance",
-            background="transparent",
-            height=200
-        ).configure_axis(disable=True)
-
-        cols1 = st.columns(3)
-        with cols1[0]:
-            st.altair_chart(bars_e, use_container_width=True, theme=None)
-        with cols1[1]:
-            st.altair_chart(bars_s, use_container_width=True, theme=None)
-        with cols1[2]:
-            st.altair_chart(bars_g, use_container_width=True, theme=None)
-    
-    with cols[1]:
-        st.markdown(
-            '''
-                <div class="divider-vertical"></div>
-                <style>
-                    .divider-vertical {
-                        border-right: 2px solid white;
-                        height: 460px;
-                        margin: auto;
-                    }
-                </style>
-            ''',
-            unsafe_allow_html=True
-        )
+        title = f"<h1 class='header'>My Portfolio</h1></br>"
+        st.write(title, unsafe_allow_html=True)
+        st.divider()
+        cols = st.columns([1, 5, 1, 5, 1, 5])
+        with cols[0]:
+            st.markdown(
+                '''
+                    <div class="divider-vertical-line"></div>
+                    <style>
+                        .divider-vertical-line {
+                            border-right: 2px solid white;
+                            height: 320px;
+                            margin: auto;
+                        }
+                    </style>
+                ''',
+                unsafe_allow_html=True
+            )
+        with cols[1]:
+            if st.session_state.res_e:
+                cols[1].metric("Environment", overall_e_score, overall_e_score - st.session_state.res_e)
+            else:
+                cols[1].metric("Environment", overall_e_score)
+                st.session_state.res_e = overall_e_score
+        with cols[2]:
+            st.markdown(
+                '''
+                    <div class="divider-vertical-line"></div>
+                    <style>
+                        .divider-vertical-line {
+                            border-right: 2px solid white;
+                            height: 320px;
+                            margin: auto;
+                        }
+                    </style>
+                ''',
+                unsafe_allow_html=True
+            )
+        with cols[3]:
+            if st.session_state.res_s:
+                cols[3].metric("Social", overall_s_score, overall_s_score - st.session_state.res_s)
+            else:
+                cols[3].metric("Social", overall_s_score)
+                st.session_state.res_s = overall_s_score
+        with cols[4]:
+            st.markdown(
+                '''
+                    <div class="divider-vertical-line"></div>
+                    <style>
+                        .divider-vertical-line {
+                            border-right: 2px solid white;
+                            height: 90px;
+                            margin: auto;
+                        }
+                    </style>
+                ''',
+                unsafe_allow_html=True
+            )
+        with cols[5]:
+            if st.session_state.res_g:
+                cols[5].metric("Governance", overall_g_score, overall_g_score - st.session_state.res_g)
+            else:
+                cols[5].metric("Governance", overall_g_score)
+                st.session_state.res_g = overall_g_score
         
-    with cols[2]:
-            news = fetch_trending_news(stocks)
-            news_bar = "<div class='news'><div class='news-header'>Trending News</div>"
-            news_html = ""
-            for article in news:
-                news_html += f"""
-                    <div class="news-item">
-                        <a href="{article['url']}" target="_blank"><strong>{article['title'][:50]}</strong></a><br>
-                        <small>{article['source']} - {datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ').strftime("%H:%M %d/%m/%Y")}</small>
-                    </div><hr class="thin-divider">"""
-            news_bar += '<div class="news-bar">'
-            news_bar += news_html
-            news_bar += "</div></div>"
-            st.markdown(news_bar, unsafe_allow_html=True)
+        st.divider()
+
+        data = {
+            "Stocks": stocks,
+            "Environment": e_scores,
+            "Social": s_scores,
+            "Governance": g_scores
+        }
+
+        if "data" not in st.session_state:
+            st.session_state.data = data
+
+        cols = st.columns([5, 1, 5])
+        with cols[0]:
+            st.table(data)
+
+            source = alt.pd.DataFrame(data)
+            bars_e = alt.Chart(source).mark_bar().encode(
+                x='Stocks',
+                y='Environment'
+            ).properties(
+                title="Environment",
+                background="transparent",
+                height=200
+            ).configure_axis(disable=True)
+            bars_s = alt.Chart(source).mark_bar().encode(
+                x='Stocks',
+                y='Social'
+            ).properties(
+                title="Social",
+                background="transparent",
+                height=200
+            ).configure_axis(disable=True)
+            bars_g = alt.Chart(source).mark_bar().encode(
+                x='Stocks',
+                y='Governance'
+            ).properties(
+                title="Governance",
+                background="transparent",
+                height=200
+            ).configure_axis(disable=True)
+
+            cols1 = st.columns(3)
+            with cols1[0]:
+                st.altair_chart(bars_e, use_container_width=True, theme=None)
+            with cols1[1]:
+                st.altair_chart(bars_s, use_container_width=True, theme=None)
+            with cols1[2]:
+                st.altair_chart(bars_g, use_container_width=True, theme=None)
+        
+        with cols[1]:
+            st.markdown(
+                '''
+                    <div class="divider-vertical"></div>
+                    <style>
+                        .divider-vertical {
+                            border-right: 2px solid white;
+                            height: 460px;
+                            margin: auto;
+                        }
+                    </style>
+                ''',
+                unsafe_allow_html=True
+            )
+            
+        with cols[2]:
+                news = fetch_trending_news(stocks)
+                news_bar = "<div class='news'><div class='news-header'>Trending News</div>"
+                news_html = ""
+                for article in news:
+                    news_html += f"""
+                        <div class="news-item">
+                            <a href="{article['url']}" target="_blank"><strong>{article['title'][:50]}</strong></a><br>
+                            <small>{article['source']} - {datetime.strptime(article['publishedAt'], '%Y-%m-%dT%H:%M:%SZ').strftime("%H:%M %d/%m/%Y")}</small>
+                        </div><hr class="thin-divider">"""
+                news_bar += '<div class="news-bar">'
+                news_bar += news_html
+                news_bar += "</div></div>"
+                st.markdown(news_bar, unsafe_allow_html=True)
+    except Exception as e:
+        print(e)
 
 def main():
     st.set_page_config(page_title="ESG Edge", layout="centered", initial_sidebar_state="collapsed")
